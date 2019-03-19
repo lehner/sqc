035c43365083854a81f26471adb127e42c345df2
Christoph Lehner <christoph@lhnr.de>
2019-03-19 10:15:07 +0100

    GHZ

---
+++ example-ghz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sqc
+
+Nbits=3
+
+# Create Greenberger-Horne-Zeilinger state
+s=sqc.operator(Nbits).H(0).CNOT(0,1).CNOT(0,2) * sqc.state(Nbits)
+
+# Print state
+print "GHZ state\n", s
+
+# Perform 10 measurements
+for n in range(10):
+    s0,v0=s.measure(0)
+    s1,v1=s0.measure(1)
+    s2,v2=s1.measure(2)
+    
+    # And print results of state and classical bits
+    print "Measurement #", n, " ", s2, v2, v1, v0
