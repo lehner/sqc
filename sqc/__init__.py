e4af95b277bdd81b47e3fa2fcc1540fe8d015a07
Christoph Lehner <christoph@lhnr.de>
2019-04-30 07:51:10 +0200

    add sample function

--- sqc/__init__.py
+++ sqc/__init__.py
@@ -7,3 +7,18 @@ from sqc.operator import operator
 
 def seed(s):
     np.random.seed(s)
+
+def sample(s, n):
+    rc={}
+    for i in range(n):
+        v=0
+        s0=s
+        for j in range(s.nbits):
+            s1,v1=s0.measure(j)
+            v=v + 2**j * v1
+            s0=s1
+        if not v in rc:
+            rc[v]=1
+        else:
+            rc[v]+=1
+    return rc
