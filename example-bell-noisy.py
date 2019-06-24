302542fc9529f1c117c8eca6a52101c0c4e898cf
Christoph Lehner <christoph@lhnr.de>
2019-06-24 21:17:17 +0200

    noise

--- example-bell-noisy.py
+++ example-bell-noisy.py
@@ -8,15 +8,15 @@ op=sqc.operator(Nbits).H(0).CNOT(0,1)
 
 # Create noise model
 nm=sqc.noise.model.simple(
-    T1 = 10000,
-    gate_times = { "X" : 0.2, "CNOT" : 0.5, "Rz" : 0.7, "H": 0.3 }, # times are in units of mus
-    qubit_readout_errors = [ 0.1, 0.0 ],
-    gate_depolarization_p = { "X" : 0.0, "CNOT" : 0.0, "Rz" : 0.0, "H" : 0.0 }
+    T1 = 50,
+    gate_times = { "X" : 0.5, "CNOT" : 0.9, "Rz" : 0.5, "H": 0.5 },
+    qubit_readout_errors = [ 0.04, 0.05 ],
+    gate_depolarization_p = { "X" : 0.04, "CNOT" : 0.06, "Rz" : 0.0, "H" : 0.03 }
 )
 
 # Initial state
 s0=sqc.state(Nbits)
 
 # Sample
-res=sqc.noise.sample(nm,op,s0,100)
+res=sqc.noise.sample(nm,op,s0,1000)
 print(res)
